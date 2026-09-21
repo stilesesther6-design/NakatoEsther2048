0e6ed168c661a6da1bf03b1d434cacd72bd4c080
stilesesther6-design <stilesesther6@gmail.com>
2026-09-21 19:37:00 +0600

    Add files via upload

---
+++ Exe1_Nakato Esther.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    ' Private field
+    Private _balance As Decimal
+
+    ' Read-only Balance property
+    Public ReadOnly Property Balance As Decimal
+        Get
+            Return _balance
+        End Get
+    End Property
+
+    ' Deposit method
+    Public Sub Deposit(amount As Decimal)
+
+        If amount < 0 Then
+            Throw New ArgumentException("Deposit amount cannot be negative.")
+        End If
+
+        _balance += amount
+
+    End Sub
+
+    ' Withdraw method
+    Public Sub Withdraw(amount As Decimal)
+
+        If amount < 0 Then
+            Throw New ArgumentException("Withdrawal amount cannot be negative.")
+        End If
+
+        If amount > _balance Then
+            Throw New InvalidOperationException("Insufficient funds.")
+        End If
+
+        _balance -= amount
+
+    End Sub
+End Class
